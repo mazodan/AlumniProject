02383171ea173d1a9b94d8ac1a5dcdf59b93c493
Jasper Onza <jasperonza@gmail.com>
2020-06-07 15:00:24 +0800

    modified user login UI

--- app/Views/user_login.php
+++ app/Views/user_login.php
@@ -8,13 +8,47 @@
     <title>Alumnus Sign-in - Alumni Tracking System</title>
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card card-signin my-5">
-            <div class="card-body">
-                <h5 class="card-title text-center">Alumnus Sign In</h5>
-                <form class="form-signin">
+    <!-- Header -->
+    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="https://spup.edu.ph/wp-content/uploads/2019/05/favicon.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+        Alumni Tracking System
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav> -->
+
+    <!-- Content -->
+    <div class="container-fluid" id="m-top">
+        <div class="container py-5 px-5 shadow" style="max-width: 500px;">
+            <div class="form-group">
+                <div class="media">
+                    <img src="https://spup.edu.ph/wp-content/uploads/2019/05/favicon.png" width="50" height="50" class="mr-3" alt="" loading="lazy">
+                    <div class="media-body">
+                        St. Paul University Philippines<br>
+                        Tuguegarao City, Cagayan 3500
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3"><h1>Alumnus Sign in</h1></div>
+            <form class="form-signin">
                 <div class="form-label-group">
                     <input type="input" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
                     <label for="inputUsername">Username</label>
@@ -24,21 +58,36 @@
                     <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
                     <label for="inputPassword">Password</label>
                 </div>
-
-                <div class="custom-control custom-checkbox mb-3">
-                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                    <label class="custom-control-label" for="customCheck1">Stay signed in</label>
+                
+                <button class="btn btn-lg btn-success btn-block text-uppercase" type="submit">Sign in</button>
+                
+                <div class="row text-center mt-3">
+                    <div class="col">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customCheck1">
+                            <label class="custom-control-label" for="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="custom-control">
+                            <a href="#">Forgot password&#63;</a>
+                        </div>
+                    </div>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-                <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit">Forgot Password</button>
-                <hr class="my-4">
-                <a href="adminlogin" class="btn btn-lg btn-google btn-block text-uppercase text-white" >Administrator Sign in</a>
-                </form>
-            </div>
-            </div>
-        </div>
+                <hr class="my-3">
+                <a href="adminlogin">Administrator sign in</a>
+            </form>
+
         </div>
     </div>
+
+    <!-- Footer -->
+    <!-- <footer class="footer" style="border: solid blue 1px;">
+      <div class="container">
+        &copy; 2019
+      </div>
+    </footer> -->
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
 </body>
 </html>
