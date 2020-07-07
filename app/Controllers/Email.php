4669048527d064e31835ec30cee1534800a95c1f
Jasper Onza <jasperonza@gmail.com>
2020-07-07 10:58:41 +0800

    added sending email functionality

---
+++ app/Controllers/Email.php
@@ -0,0 +1,31 @@
+<?php namespace App\Controllers;
+
+class Email extends BaseController{
+
+  public function email_view(){
+    $session = session();
+    $_SESSION['alumni_id'] = '123';
+    $_SESSION['alumni_firstname'] = 'Alumni';
+    $_SESSION['alumni_lastname'] = 'User';
+    return view('send_email');
+  }
+
+  public function action_send_email(){
+    $session = session();
+    $email = \Config\Services::email();
+
+    if(!isset($_POST['sendmail'])){
+      return view('errors/html/error_404');
+    }else{
+      $email->setFrom('alumni@jasperonza.com', 'Alumni Name');
+      $email->setTo('jasperonza@gmail.com');
+      $email->setSubject('Email Test');
+      $email->setMessage('Test email from localhost codeigniter');
+      $email->send();
+
+      echo 'email sent';
+      // return view('send_email');
+    }
+    
+  }
+}
